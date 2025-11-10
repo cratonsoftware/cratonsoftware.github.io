2f1841b6a02c1dd5e61e29a873f834fd5c551ba8
Luiz Fernando <f3rn.luiz@gmail.com>
2025-11-10 10:56:49 -0300

    docs(Configurações): ajustes dos providers e no estilo

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,13 @@
+import { registerLocaleData } from '@angular/common';
+import ptBr from '@angular/common/locales/pt';
+import { DEFAULT_CURRENCY_CODE, LOCALE_ID, provideZoneChangeDetection } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
+import { provideRouter } from '@angular/router';
 import { AppComponent } from './app/app.component';
+import { routes } from './app/app.routes';
 
-bootstrapApplication(AppComponent, appConfig).catch((err) => console.error(err));
+registerLocaleData(ptBr);
+
+bootstrapApplication(AppComponent, {
+	providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), { provide: LOCALE_ID, useValue: 'pt-BR' }, { provide: DEFAULT_CURRENCY_CODE, useValue: 'BRL' }],
+}).catch((err) => console.error(err));
